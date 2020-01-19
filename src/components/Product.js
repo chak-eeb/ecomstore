08bc99ca3afcae2c8c9e105f2b995d862456c4fa
chak-eeb <chak_eeb@yahoo.com>
2020-01-19 18:46:39 -0500

    set up all the routes components

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Product extends Component {
+	render() {
+		return (
+			<div>
+				<h4>hello fr product</h4>
+			</div>
+		);
+	}
+}
